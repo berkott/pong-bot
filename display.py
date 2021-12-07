cd9b1f86e83fcbb2c797c1f8abe2dd4bd9747be1
Berk Ott <berkott@gmail.com>
2021-12-07 23:20:23 +0000

    lfg display works broooooo

--- display.py
+++ display.py
@@ -14,9 +14,10 @@ import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
+import time
 
 # Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
+oled_reset = digitalio.DigitalInOut(board.D1)
 
 # Change these
 # to the right size for your display!
@@ -24,14 +25,18 @@ WIDTH = 128
 HEIGHT = 64  # Change to 64 if needed
 BORDER = 5
 
+# Use for I2C
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3d, reset = oled_reset)
+
 # Use for SPI
-spi = board.SPI()
-oled_cs = digitalio.DigitalInOut(board.D5)
-oled_dc = digitalio.DigitalInOut(board.D6)
-oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
+# spi = board.SPI()
+# oled_cs = digitalio.DigitalInOut(board.D6)
+# oled_dc = digitalio.DigitalInOut(board.D4)
+# oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
 
 # Clear display.
-oled.fill(0)
+oled.fill(1)
 oled.show()
 
 # Create blank image for drawing.
